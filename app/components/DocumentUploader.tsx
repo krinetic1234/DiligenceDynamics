bfceea788115c6a4559380fd933a9cc7eb26952b
Justin Lim <jlim13295@gmail.com>
2024-01-30 17:59:11 -0800

    kind of fix document uploader so its not huge

--- app/components/DocumentUploader.tsx
+++ app/components/DocumentUploader.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import styles from '../styles/DocumentUploader.module.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/globals.css';
+
 
 const DocumentUploader = () => {
+  
   return (
-    <div className={styles.uploaderContainer}>
-      <input
-        type="file"
-        className={styles.fileInput}
-        accept=".pdf,.doc,.docx,.txt"
-      />
+    <div className="mt-5 w50">
+      <div className="custom-file">
+        <input type="file" className="custom-file-input" id="customFile" />
+        {/* <label className="custom-file-label" htmlFor="customFile">
+          Choose file
+        </label> */}
+      </div>
     </div>
   );
 };
