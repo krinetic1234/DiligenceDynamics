c761b89f341889d63ad8d8f3482b77485ab319e9
Jakob Nordhagen <jakobn@stanford.edu>
2024-02-15 06:02:01 -0800

    added login-signup button to sidebar

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("cf59894005b25fc8")
+/******/ 		__webpack_require__.h = () => ("22fb910ce2908b71")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
