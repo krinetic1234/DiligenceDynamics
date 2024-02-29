597302b573df5cc27f1e8c42dca6f0682d430a89
Krish Maniar <56781484+krinetic1234@users.noreply.github.com>
2024-02-29 10:28:59 -0800

    change scrape.py

--- ai/sentiment/scrape.py
+++ ai/sentiment/scrape.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 from test_ft import ai_output
 import praw
 import nltk
+from transformers import AutoModelForCausalLM, AutoTokenizer
 nltk.download('vader_lexicon')
+
 # reddit credentials
 reddit = praw.Reddit(
     client_id='r2CWL-NH4e0nEe9sf6Czbw',
@@ -17,82 +19,104 @@ reddit = praw.Reddit(
 newsapi_key = 'eab52d8b241d40eaba0b3fc166139863'
 company_name = 'NVIDIA'
 
-# together AI models 
-krish_model_ft = "Krish/Mixtral-8x7B-v0.1-dildyn-ft-2024-02-25-02-09-30"
-mistral_model_inf = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+# set up mxstral model
+device = "cuda" 
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
+
+# AI models 
+# krish_model_ft = "Krish/Mixtral-8x7B-v0.1-dildyn-ft-2024-02-25-02-09-30"
+# mistral_model_inf = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
 # fetch subreddit sentiment
-def fetch_reddit_sentiment(reddit, company_name):
+def fetch_reddit_sentiment(company_name, reddit=reddit):
     subreddit = reddit.subreddit(company_name)
     top_posts = subreddit.top(time_filter='week', limit=20)
     sia = SentimentIntensityAnalyzer()
 
+    titles = []
     sentiment_scores = []  
-    ai_content = ""
 
     for post in top_posts:
         title = post.title
         content = post.selftext
-        ai_content += content
         sentiment = sia.polarity_scores(content)['compound']
         adjusted_score = sentiment * 5  
+        titles.append(title)
         sentiment_scores.append(adjusted_score)  
-    
-    ai_content = ai_output(mistral_model_inf, ai_content)
-    ai_sentiment = sia.polarity_scores(ai_content)['compound'] * 5
 
-    return sentiment_scores, ai_sentiment
+    return titles, sentiment_scores
 
 # fetch news articles sentiment
-def fetch_news_sentiment(api_key, company_name):
+def fetch_news_sentiment(company_name, api_key=newsapi_key):
     url = f'https://newsapi.org/v2/everything?q={company_name}&apiKey={api_key}'
     response = requests.get(url)
     articles = response.json().get('articles', [])
     
     # sentiment analyzer (replace with an LLM or something better)
     sia = SentimentIntensityAnalyzer()
+    titles = []
     sentiment_scores = []
-    ai_description = ""
 
     for article in articles:
+        title = article.get('title', '')
         description = article.get('content', '') # consider using 'description'
-        ai_description += description
         if description:
             sentiment_score = sia.polarity_scores(description)['compound']
             adjusted_score = sentiment_score * 5
             sentiment_scores.append(adjusted_score)
         else:
             sentiment_scores.append(None)  # no score
-
+        titles.append(title)
     
-    ai_description = ai_output(mistral_model_inf, ai_description)
-    ai_sentiment = sia.polarity_scores(ai_description)['compound'] * 5
-    
-    return sentiment_scores, ai_sentiment
+    return titles, sentiment_score
 
+def fetch_ai_response(titles, scores):
+    prompt = "You are a financial analyst that needs to describe the general distribution of sentiment of a specific company based on its individual articles. Here are the latest titles and their sentiment scores:\n"
+    for title, score in zip(titles, scores):
+        prompt += f"Title: {title}, Sentiment Score: {score}\n"
 
-news_scores, news_ai_sentiment = fetch_news_sentiment(newsapi_key, company_name)
-reddit_scores, reddit_ai_sentiment = fetch_reddit_sentiment(reddit, company_name)
+    model_inputs = tokenizer([prompt], return_tensors="pt").to(device)
+    model.to(device)
+    generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample=True)
+    return tokenizer.batch_decode(generated_ids)[0]
 
-# filtering both news and reddit
+# getting scores and filtering them
+news_tt, news_scores = fetch_news_sentiment(newsapi_key, company_name)
 news_scores_filtered = list(filter(lambda x: x is not None, news_scores))
+
+reddit_tt, reddit_scores = fetch_reddit_sentiment(reddit, company_name)
 reddit_scores_filtered = list(filter(lambda x: x is not None, reddit_scores))
 
 # plotting function
-def plot_sentiment_scores(scores, title, file_name):
+def plot_sentiment_scores(scores, file_name):
     plt.figure(figsize=(10, 6))
+    path = f"/Users/krish/Desktop/CS/Projects/DiligenceDynamics/app/static/{file_name}"
     plt.hist(scores, bins=20, color='skyblue')
-    plt.title(f'Distribution of Sentiment Scores for {title}')
+    plt.title(f'Distribution of Sentiment Scores for {company_name}')
     plt.xlabel('Sentiment Score (-5 to 5)')
     plt.ylabel('Number of Posts/Articles')
     plt.grid(axis='y', alpha=0.75)
-    plt.savefig(f"sentiment/viz/{file_name}")
-    plt.close()  
+    plt.savefig(path)
+    return path
 
-# individual histograms
-plot_sentiment_scores(news_scores_filtered, 'NVIDIA News Articles', 'NVDA_news_sentiment_distribution.png')
-plot_sentiment_scores(reddit_scores_filtered, 'NVIDIA Reddit Posts', 'NVDA_reddit_sentiment_distribution.png')
+# api function calls
+def news_results(company_name):
+    file_name = f"{company_name} news"
+    path = plot_sentiment_scores(news_scores_filtered, file_name)
+    news_ai = fetch_ai_response(news_tt, news_scores_filtered)
+    return path, news_ai
 
-# combined histogram
-combined_scores = news_scores_filtered + reddit_scores_filtered
-plot_sentiment_scores(combined_scores, 'NVIDIA News and Reddit Combined', 'NVDA_combined_sentiment_distribution.png')
+def reddit_results(company_name):
+    file_name = f"{company_name} reddit"
+    path = plot_sentiment_scores(reddit_scores_filtered, file_name)
+    reddit_ai = fetch_ai_response(reddit_tt, reddit_scores_filtered)
+    return path, reddit_ai
+
+# # individual histograms
+# plot_sentiment_scores(news_scores_filtered, 'Apple News Articles', 'Apple_news_sentiment_distribution.png')
+# plot_sentiment_scores(reddit_scores_filtered, 'Apple Reddit Posts', 'NVDA_reddit_sentiment_distribution.png')
+
+# # combined histogram
+# combined_scores = news_scores_filtered + reddit_scores_filtered
+# plot_sentiment_scores(combined_scores, 'NVIDIA News and Reddit Combined', 'NVDA_combined_sentiment_distribution.png')
