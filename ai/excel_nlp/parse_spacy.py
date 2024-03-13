92b637c28cbcc47f6cd3b0692fb03da4bb18a33c
Krish Maniar <56781484+krinetic1234@users.noreply.github.com>
2024-03-13 16:06:27 -0700

    Update parse_spacy.py

--- ai/excel_nlp/parse_spacy.py
+++ ai/excel_nlp/parse_spacy.py
@@ -21,14 +21,12 @@ def generate_graph(sheet, title, data_range):
     chart.add_data(data, titles_from_data=True)
     sheet.add_chart(chart, "E2")
 
-def main(instruction):
+def fetch_graph(instruction):
     numbers = process_instruction(instruction)
     wb = openpyxl.Workbook()
     sheet = wb.active
     create_excel_list(numbers, sheet)
     generate_graph(sheet, "Stocks over Time", {'min_col': 1, 'min_row': 1, 'max_row': 4, 'max_col': 1})
-    wb.save("SalesData.xlsx")
-
-if __name__ == "__main__":
-    instruction = "Apple recently has had fluctuating stock prices. Create a list with 80, 82, and 90."
-    main(instruction)
+    file_path = "/Users/krish/Desktop/CS/Projects/DiligenceDynamics/public/Data.csv"
+    wb.save(file_path)
+    return file_path
